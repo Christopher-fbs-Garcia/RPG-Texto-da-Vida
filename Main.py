bde4d9ff85a7e385e9f6c482b20c3b941c0172b2
Christopher-fbs-Garcia <christopher.fbs.garcia@gmail.com>
2025-08-14 13:06:59 -0300

    gerenciamento de efeitos negativos

--- Main.py
+++ Main.py
@@ -12,33 +12,33 @@ my_player.name = input("Qual é o seu nome: ")
 my_player.skills = Skill.skills()
 
 
-def about_professional():
-    professionals_player = []
-    final_professionals = []
+# def about_professional():
+#     professionals_player = []
+#     final_professionals = []
 
-    professionals = {
-        "Lógico-Matemática" : ("Engenheiro", "Físico", "Mecânico"),  # 1
-        "Interpessoal" : ("político", "jornalista", "Professor"),  # 2
-        "Naturalista" : ("Biológo", "Geólogo", "Veterinário."),  # 3
-        "Musical" : ("Cantor", "Violinista", "Pianista"), # 4
-        "Cinestésica Corporal" : ("Treinador", "Jogador de basquete", "Dançarino"), # 5
-        "Linguística" : ("Dublador", "Escrito", "Palestrantes"), # 6
-        "Tecnologia" : ("Hacker", "Programador Java", ""), # 7
-        "Religioso" : ("Padre", "Teólogo", "Papa"), # 8
-        "Negócios" : ("Empreededor", "Ecnonomista") # 9
-    }  
+#     professionals = {
+#         "Lógico-Matemática" : ("Engenheiro", "Físico", "Mecânico"),  # 1
+#         "Interpessoal" : ("político", "jornalista", "Professor"),  # 2
+#         "Naturalista" : ("Biológo", "Geólogo", "Veterinário."),  # 3
+#         "Musical" : ("Cantor", "Violinista", "Pianista"), # 4
+#         "Cinestésica Corporal" : ("Treinador", "Jogador de basquete", "Dançarino"), # 5
+#         "Linguística" : ("Dublador", "Escrito", "Palestrantes"), # 6
+#         "Tecnologia" : ("Hacker", "Programador Java", ""), # 7
+#         "Religioso" : ("Padre", "Teólogo", "Papa"), # 8
+#         "Negócios" : ("Empreededor", "Ecnonomista") # 9
+#     }  
 
-    for key, value in professionals.items():
-        for skill in my_player.skills:
+#     for key, value in professionals.items():
+#         for skill in my_player.skills:
 
-            if key == skill:
-                professionals_player.append(value)
+#             if key == skill:
+#                 professionals_player.append(value)
     
-    for pp in professionals_player:
-        for p in pp:
-            final_professionals.append(p)
+#     for pp in professionals_player:
+#         for p in pp:
+#             final_professionals.append(p)
     
-    return final_professionals
+#     return final_professionals
 
     
 
---
+++ PlayerFile/good_events/sick.py
@@ -0,0 +1,6 @@
+
+class Sick:
+    def __init__(self, name_sick, negative_physics, time_sick):
+        self.name_sick = name_sick
+        self.negative_physics = int(negative_physics)
+        self.time_sick = time_sick
